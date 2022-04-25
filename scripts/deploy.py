79a285bf7780bed52f224d97c2c9c1ad7c3da3a8
Datuna <datuna@Datunas-MacBook-Air.local>
2022-04-25 11:18:05 +0400

    initial commit

---
+++ scripts/deploy.py
@@ -0,0 +1,12 @@
+# from brownie import accounts, FundMe, network, config
+
+
+# def get_accounts():
+#     if network.is_local == True:
+#         return accounts[0]
+#     else:
+#         return config["wallets"]["from_key"]
+
+
+# def main():
+#     get_accounts()
